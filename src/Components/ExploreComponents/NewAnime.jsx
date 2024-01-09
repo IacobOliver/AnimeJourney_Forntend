c758daa0eeb879bd603762a04fb5c28b623750fa
IacobOliver <olialexander08@gmail.com>
2024-01-09 23:09:59 +0200

    test 2 requests

--- src/Components/ExploreComponents/NewAnime.jsx
+++ src/Components/ExploreComponents/NewAnime.jsx
@@ -19,7 +19,7 @@ export default function AnimeList() {
 
 
     useEffect(() => {
-        fetch(`http://localhost:8080/newAnimeSeasons/getAnime`, {
+        fetch(`http://13.48.56.38:80/newAnimeSeasons/getAnime`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
